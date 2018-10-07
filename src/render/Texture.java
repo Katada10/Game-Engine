81c38d9ffb8e066faf8289f53f4fb01891925b91
katad <katadafreije@gmail.com>
2018-10-07 10:49:20 -0400

    Model loads, need to fix textures

---
+++ src/render/Texture.java
@@ -0,0 +1,30 @@
+package render;
+
+public class Texture {
+
+	private float x, y;
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public Texture(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+}

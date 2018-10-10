c625533249f07d60bcb10e113c0ad92f1513ff98
katad <katadafreije@gmail.com>
2018-10-10 00:32:20 -0400

    Need to implement drawinstanced

--- src/render/Renderer.java
+++ src/render/Renderer.java
@@ -1,9 +1,13 @@
 package render;
 
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.GL31;
 
 import data.BufferManager;
 import data.GameObject;
@@ -17,85 +21,84 @@ public class Renderer {
 
 	private List<GameObject> objects = new ArrayList<>();
 	ShaderRenderer r;
-	
+
 	ShaderManager sm;
-	
+
 	Camera cam;
-	
+
 	public Renderer() {
 		r = new ShaderRenderer("vert.txt", "frag.txt");
-		
+
 		cam = new Camera();
 		sm = new ShaderManager(r.getProgId());
-		
+
 		BufferManager.init();
-		
-		GameObject o = Loader.LoadObj("cube", "mario.jpg");		
+
+		GameObject o = Loader.LoadObj("cube", "mario.jpg");
 		GameObject o2 = Loader.LoadObj("cube", "mario.jpg");
-		
+
 		addObject(o2);
 		addObject(o);
 	}
-	
-	public void bind(GameObject obj)
-	{
+
+	public void bind(Model m) {
 		GL30.glEnableVertexAttribArray(0);
 		GL30.glEnableVertexAttribArray(1);
 		GL30.glEnableVertexAttribArray(2);
-		
-		Model m = BufferManager.models.get(obj.getModelId());
+
 		GL30.glBindBuffer(GL30.GL_ELEMENT_ARRAY_BUFFER, m.getEbo());
 	}
-	
-	public void draw(GameObject o)
-	{
-		BufferManager.start();
-		
-		cam.Move();
-		
-		sm.render(cam);
-		
-		Model m = BufferManager.models.get(o.getModelId());
+
+	public void draw(GameObject o) {
 		sm.render(o);
-		
-		bind(o);
-		GL30.glDrawElements(GL30.GL_TRIANGLES, m.getIndices().length, GL30.GL_UNSIGNED_INT, 0);
-		finish();
+
+		Model m = BufferManager.models.get(o.getModelId());
+
+
+		//Convert to buffers
+		//GL31.glDrawElementsInstanced(GL30.GL_TRIANGLES, buf, 1);
 	}
-	
-	public void render()
-	{
+
+	public void render() {
+		BufferManager.start();
+
+		cam.Move();
+
+		sm.render(cam);
+
+		for (Model m : BufferManager.modList) {
+			bind(m);
+		}
+
+		//Instead of arrays, change to instanced
 		for (GameObject obj : objects) {
 			draw(obj);
 		}
+
+		finish();
 	}
-	
-	public void finish()
-	{
+
+	public void finish() {
 		GL30.glDisableVertexAttribArray(0);
 		GL30.glDisableVertexAttribArray(1);
 		GL30.glDisableVertexAttribArray(2);
-		
+
 		GL30.glBindVertexArray(0);
 		GL30.glBindVertexArray(1);
 		GL30.glBindVertexArray(2);
-		
+
 		GL30.glBindTexture(GL30.GL_TEXTURE_2D_ARRAY, 0);
 	}
-	
-	public void destroy()
-	{
+
+	public void destroy() {
 		BufferManager.cleanup();
-		for(GameObject o : objects)
-		{
+		for (GameObject o : objects) {
 			o.CleanUp();
 		}
 	}
-	
 
-	public void addObject(GameObject o)
-	{
+	public void addObject(GameObject o) {
 		objects.add(o);
 	}
-	
+
 }
